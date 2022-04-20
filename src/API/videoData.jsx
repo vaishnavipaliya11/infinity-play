ee1340611260ab9890b8ca48728e38ecd113cd7a
vaishnavi754 <81965405+vaishnavipaliya11@users.noreply.github.com>
2022-04-20 14:59:59 +0530

    feat: data fetched and displayed

---
+++ src/API/videoData.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const videoData = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios("/api/videos")
+    .then((res) => setData(res.data.videos));
+  }, []);
+
+  return { data };
+};
+
+export { videoData };
--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,73 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "c0VzgM8gkUA",
+    title: "Wollen Caps",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "Make wollen caps, clothes and many more with simple texhniques.",
+    creator: "India",
+    category: "weaving",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "pgXeFSpj7oQ",
+    title: "Clay Arts",
+    description:"This video is a clay art tutorial on how to make 3D Clay artwork on any surface. Easy clay art for beginners and also I have shown how to work with clay step by step. These air dry clay crafts are easy & fun to make",
+    creator: "India",
+    category: "clay",
+  },
+  
+  {
+    _id: "S9DnGzgyoX8",
+    title: "Lamps Decoration",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Enlighten your home with this beautiful lamps.Make your everyday a diwali day",
+    creator: "India",
+    category: "decoration",
+  },
+  {
+    _id: "0WMn_ywN6Qk",
+    title: "Wollen Jackets",
+    description:
+      "Make wollen caps, clothes and many more with simple texhniques.",
+    creator: "India",
+    category: "weaving",
+  },
+ 
+  {
+    _id: "oxuum_F3mXw",
+    title: "Wall Paintings",
+    description:
+      "13 Best.. DiY Teen Girls Room Decor Ideas - That Will Make your Home Beautiful Ever Than Before",
+    creator: "India",
+    category: "wall",
+  },
+  {
+    _id: "EozxkVBOEBk",
+    title: "Wall Decors",
+    description:
+      "In this video, I am sharing how to decorate the blank wall? Modern living room Wall decorating ideas for your home interior. Wooden wall green grass Home decor and led lights with Wall panels. Accent wall wood sculptures decoration for empty wall. ",
+    creator: "India",
+    category: "wall",
+  },
+  {
+    _id: "IwyJ2yRrfbM",
+    title: "Paper Flowers",
+    description:
+      "Decor your home with easy, quick and budget friendly paper flowers.",
+    creator: "India",
+    category: "decoration",
+  },
+ 
+  {
+    _id: "cCK_92tWde0",
+    title: "Clay Arts",
+    description:
+      "In this video, I am sharing how to make beautiful roses with simple clay art",
+    creator: "India",
+    category: "clay",
   },
 ];
+
+
+
+
