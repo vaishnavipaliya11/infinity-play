a8ae4287650c743b735b86e2ce8f1ac3f90b5438
vaishnavi754 <81965405+vaishnavipaliya11@users.noreply.github.com>
2022-04-24 14:39:53 +0530

    style: styled single page

--- src/Pages/SingleProduct/SingleProduct.jsx
+++ src/Pages/SingleProduct/SingleProduct.jsx
@@ -1,33 +1,53 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { videoData } from "../../API/videoData";
-import "./SingleProduct.css"
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { MdPlaylistAdd, MdWatchLater } from "react-icons/md";
+import "./SingleProduct.css";
+import { getVideo } from "../../Utils/getVideo";
+import { useState } from "react";
 
 const SingleProduct = () => {
-   const {video_id} = useParams()
-   const {data} = videoData()
+  const { video_id } = useParams();
 
-   console.log('from single page', data);
+  const [singleVideoData, setSingleVideoData] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const resVideo = await getVideo(video_id);
+      setSingleVideoData(resVideo);
+    })();
+  }, [video_id]);
+
   return (
-    <div className='single-video-container'>
-    <div className='video-display-container'>
-    <iframe
-     width="100%"
-     height="500px"
-     src={`https://www.youtube.com/embed/${video_id}`}
-     title="YouTube video player"
-     frameBorder="0"
-     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-     allowFullScreen="">
-    </iframe>
-
-    </div>
-
     <div>
-  
-    </div>
-    </div>
-  )
-}
+      <div className="video-container">
+        <div className="single-video-container">
+          <iframe
+            width="100%"
+            height="500px"
+            src={`https://www.youtube.com/embed/${video_id}`}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen=""
+          ></iframe>
+        </div>
+      </div>
 
-export  {SingleProduct}
+      <div className="video-btn-title-set">
+        <p className="video-title">{singleVideoData.title}</p>
+        <div className="video-btns">
+          <button className="btns">
+            <MdPlaylistAdd />
+          </button>
+          <button>
+            <MdWatchLater />
+          </button>
+        </div>
+      </div>
+      <p className="video-description">{singleVideoData.description}</p>
+    </div>
+  );
+};
+
+export { SingleProduct };
