1994c0829319d0baf30a522e183e1ff87001beb5
vaishnavi754 <81965405+vaishnavipaliya11@users.noreply.github.com>
2022-04-25 22:18:49 +0530

    refactor: removed unwanted code

--- src/Pages/Watch/Watch.jsx
+++ src/Pages/Watch/Watch.jsx
@@ -1,38 +1,35 @@
-import React from 'react'
-import { useWatchLater } from '../../context/watchContext';
-import {useAuth} from "../../context/authContext";
-import { VideoCard } from '../../Components/VideoCard/VideoCard';
-import "./Watch.css"
-import { deleteWatchLater } from '../../Utils/deleteWatchLater';
+import React from "react";
+import { useWatchLater } from "../../context/watchContext";
+import { VideoCard } from "../../Components/VideoCard/VideoCard";
+import "./Watch.css";
+import { deleteWatchLater } from "../../Utils/deleteWatchLater";
 
 const WatchLater = () => {
-
   const { watchLaterState } = useWatchLater();
-  const {auth}= useAuth();
-  const {watchLater}= watchLaterState;
+  const { watchLater } = watchLaterState;
   const { watchLaterDispatch } = useWatchLater();
 
   return (
-    <div className='watchLater-container'>
-    <h2>This is Watch later page.</h2>
+    <div className="watchLater-container">
+      <h2>This is Watch later page.</h2>
 
-    {watchLater.map((video)=>{
-      const {_id} = video
-      return(
-        <div className='video-map-container'>
-        <VideoCard video={video}/>
-        <button className='video-delete-btn'
-        onClick={() =>
-           deleteWatchLater(_id, watchLaterDispatch)
-          
-        }> Remove </button>
-        </div>
-        
-      )
-    })}
-  
+      {watchLater.map((video) => {
+        const { _id } = video;
+        return (
+          <div className="video-map-container">
+            <VideoCard video={video} />
+            <button
+              className="video-delete-btn"
+              onClick={() => deleteWatchLater(_id, watchLaterDispatch)}
+            >
+              {" "}
+              Remove{" "}
+            </button>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export  {WatchLater}
+export { WatchLater };
