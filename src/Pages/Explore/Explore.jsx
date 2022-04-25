e4c0a21bd133f9e0bb3b2091dda65908604480e4
vaishnavi754 <81965405+vaishnavipaliya11@users.noreply.github.com>
2022-04-25 21:18:48 +0530

    feat: video card added

--- src/Pages/Explore/Explore.jsx
+++ src/Pages/Explore/Explore.jsx
@@ -8,59 +8,24 @@ import { videoData } from "../../API/videoData";
 import { useWatchLater } from "../../context/watchContext";
 import { useAuth } from "../../context/authContext";
 import { addToWatchLater } from "../../Utils/addToWatchLater";
-const Explore = () => {
+import {VideoCard} from "../../Components/VideoCard/VideoCard";
 
-  const {watchLaterState,watchLaterDispatch} = useWatchLater()
-  const navigate = useNavigate()
-  const {data} = videoData()
-  const {auth}= useAuth()
+const Explore = () => {
+  const { watchLaterState, watchLaterDispatch } = useWatchLater();
+  const navigate = useNavigate();
+  const { data } = videoData();
+  const { auth } = useAuth();
+
   return (
     <div className="main-container">
-      
       <div className="video-cards-container">
-
       {data.map((video)=>{
-        const {_id, title, description} = video
-        return(
-          
-          <div className="products-card-container">
-          <div className="product-card">
-            <div className="product-tumb">
-              <img  src={`https://i.ytimg.com/vi/${_id}/0.jpg`} />
-            </div>
-            <div className="product-details">
-              <span className="product-catagory">
-              <Link to={`/singlevideo/${_id}`}>
-              <p className="prod-title">{title}</p>
-              </Link>
-                
-              </span>
-
-              <div>
-                <button className="clear-btn">
-                  <h2>
-                    <MdPlaylistAdd />
-                  </h2>
-                </button>
-
-                <button className="clear-btn"
-                onClick={() =>
-                  auth
-                    ? addToWatchLater(video, watchLaterDispatch)
-                    : navigate("/login")
-                }>
-                <h3>
-                <MdWatchLater />
-                </h3>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-          
+        console.log("from explore", video);
+        return (
+          <VideoCard video={video}/>
         )
       })}
-
+        
       </div>
     </div>
   );
@@ -68,5 +33,4 @@ const Explore = () => {
 
 export { Explore };
 
-
-<VideoCard video={video}/>
+// <VideoCard video={video}/>
--- src/Pages/Watch/Watch.jsx
+++ src/Pages/Watch/Watch.jsx
@@ -1,23 +1,37 @@
 import React from 'react'
 import { useWatchLater } from '../../context/watchContext';
 import {useAuth} from "../../context/authContext";
+import { VideoCard } from '../../Components/VideoCard/VideoCard';
+import "./Watch.css"
+
 
 const WatchLater = () => {
+
   const { watchLaterState } = useWatchLater();
   const {auth}= useAuth();
   const {watchLater}= watchLaterState;
-
+  
   console.log(watchLater);
+
   return (
-    <div>
+    <div className='watchLater-container'>
     <h2>This is Watch later page.</h2>
+
     {watchLater.map((video)=>{
-    return (
-    <div>
-    {video._id}
-    </div>
-  )
-  })}
+      const {_id,title} = video
+      return(
+        <div className='video-map-container'>
+        <VideoCard video={video}/>
+        <button className='video-delete-btn'
+        onClick=
+        {deleteWatchLater(video, watchLaterDispatch)}>
+        
+        delete</button>
+        </div>
+        
+      )
+    })}
+  
     </div>
   )
 }
