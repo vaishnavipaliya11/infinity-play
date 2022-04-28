de51e23aaf2feea142a1a8ac8ffaaa8db0ed8c4f
vaishnavi754 <81965405+vaishnavipaliya11@users.noreply.github.com>
2022-04-28 16:49:33 +0530

    feat: function created

---
+++ src/Utils/addToHistory.js
@@ -0,0 +1,23 @@
+import axios from "axios"
+
+const addToHistory = async (video, historyDispatch) =>{
+
+    try {
+        const {data}= await axios({
+            method:"POST",
+            url:"",
+            headers:{
+                authorization:getUserToken(),
+            },
+            data:{
+                video:video
+            }
+        })
+
+        console.log(data);
+        // historyDispatch({type:"ADD_TO_HISTORY", payload: data.historyvideo})
+
+    } catch (error) {
+        console.log(error);
+    }
+}
